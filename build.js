33167cd0e0df8495417bb8c55738f80c3d5ec772
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2017-09-04 11:07:06 +0800

    Fix tt undefined bug

--- build.js
+++ build.js
@@ -1,60 +1,64 @@
-'use strict';
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/koa-app'); // connec
-var taskModel  = require('./models/task');
+var taskModel = require('./models/task');
 var releaseModel = require('./models/release');
 
 var pack = require('cordova-pack-old').default;
 
 var busy = false;
 
-var findTask = function(){
-    return new Promise(function(resolve,reject){
-        taskModel.find({ status: 'waiting' }, function(err, bears) {
-            if(err){
+var findTask = function () {
+    return new Promise((resolve, reject) => {
+        taskModel.find({status: 'waiting'}, (err, tasks) => {
+            if (err) {
                 reject(err);
+                return;
             }
-            if(bears.length==0){
+            if (tasks.length === 0) {
                 reject('no task');
+                return;
             }
-            resolve(bears[0]);
+            resolve(tasks[0]);
         })
     })
 }
 
-function monitor(){
+function monitor() {
     console.log('start monitor loop');
-    if(busy)
+    if (busy)
         return;
     var tt = null;
-    findTask().then(function(task){
-        busy=true;
-        task.status = "accepted";
-        task.save();
+    findTask()
+        .then(function (task) {
+            console.log('find a task');
+            busy = true;
+            task.status = "accepted";
+            task.save();
 
-        var winston = require('winston');
-        var filename = 'yigomobile/public/log/';
-        filename += task.id + '.log';
-        var fs = require('fs');
-        var stream = fs.createWriteStream(filename);
-        var file = new (winston.transports.File)({ stream: stream,    handleExceptions: true,
-            humanReadableUnhandledException: true });
-        task.winston = new (winston.Logger)({ transports: [file] });
+            var winston = require('winston');
+            var filename = 'yigomobile/public/log/';
+            filename += task.id + '.log';
+            var fs = require('fs');
+            var stream = fs.createWriteStream(filename);
+            var file = new (winston.transports.File)({
+                stream: stream, handleExceptions: true,
+                humanReadableUnhandledException: true
+            });
+            task.winston = new (winston.Logger)({transports: [file]});
 
-        task.winston.info('begin to pack ',task.id);
-        tt = task;
-        // return tt;
-        var packIns = pack(task);
-        return packIns.build();
-    })
-        .then(function(packIns){
+            task.winston.info('begin to pack ', task.id);
+            tt = task;
+            // return tt;
+            var packIns = pack(task);
+            return packIns.build();
+        }).then(function () {
         tt.status = "finished";
         tt.save();
         tt.winston.info('pack success');
         /*
-        * pack success. After that, release
-        * */
-        if(tt.appRelease){
+         * pack success. After that, release
+         * */
+        if (tt.appRelease) {
             const appPackageName = tt.appPackageName;
             const appPlatform = tt.appPlatform;
             const appVersion = tt.appVersion;
@@ -62,41 +66,41 @@ function monitor(){
             const serverPath = 'https://dev.bokesoft.com/yigomobile/public/';
             let androidLink = `${serverPath}apk/${tt.id}/${tt.appName}-${tt.appBuildType}.apk`;
             let iosLink = `${serverPath}ios/${tt.id}/index.html`;
-            switch (appPlatform){
+            switch (appPlatform) {
                 case 'android':
                     var query = {
-                        "appName":appName,
-                        "appPackageName":appPackageName,
-                        "androidVersion":appVersion,
-                        "androidLink":androidLink,
-                        'androidUpdateTime':new Date(),
+                        "appName": appName,
+                        "appPackageName": appPackageName,
+                        "androidVersion": appVersion,
+                        "androidLink": androidLink,
+                        'androidUpdateTime': new Date(),
                     };
                     break;
                 case 'ios':
                     var query = {
-                        "appName":appName,
-                        "appPackageName":appPackageName,
-                        "iosVersion":appVersion,
-                        "iosLink":iosLink,
-                        'iosUpdateTime':new Date(),
+                        "appName": appName,
+                        "appPackageName": appPackageName,
+                        "iosVersion": appVersion,
+                        "iosLink": iosLink,
+                        'iosUpdateTime': new Date(),
                     };
                     break;
             }
-            var saveData = new Promise(function(resolve,reject){
-                releaseModel.find({appPackageName:appPackageName},function(err,data){
-                    if(err){
+            var saveData = new Promise(function (resolve, reject) {
+                releaseModel.find({appPackageName: appPackageName}, function (err, data) {
+                    if (err) {
                         reject(err);
                     }
-                    if(data.length){
+                    if (data.length) {
                         //updage
-                        releaseModel.findOneAndUpdate({"appPackageName":appPackageName},query,{new: true},function(err,data){
-                            if(err){
+                        releaseModel.findOneAndUpdate({"appPackageName": appPackageName}, query, {new: true}, function (err, data) {
+                            if (err) {
                                 reject(err);
                             }
                             resolve(data);
 
                         });
-                    }else{
+                    } else {
                         //create
                         var newQuery = new releaseModel(query);
                         newQuery.save(function (err, data) {
@@ -115,36 +119,37 @@ function monitor(){
                     //generater-html
                     var fs = require('fs-extra')
                     var _ = require('underscore');
-                    fs.readFile('release/index.html', 'utf8', function(err, html){
-                        function getLocalTime(date){
+                    fs.readFile('release/index.html', 'utf8', function (err, html) {
+                        function getLocalTime(date) {
                             var a = new Date(date);
                             const year = a.getFullYear();
                             const month = a.getMonth() + 1;
                             const day = a.getDate();
-                            return [year,month,day].join('-')
+                            return [year, month, day].join('-')
                         }
+
                         var compiled = _.template(html);
-                        if(data.androidUpdateTime){
+                        if (data.androidUpdateTime) {
                             const temp = getLocalTime(data.androidUpdateTime);
                             data.androidUpdateTime = temp.toString();
 
                         }
-                        if(data.iosUpdateTime){
+                        if (data.iosUpdateTime) {
                             const temp = getLocalTime(data.iosUpdateTime);
 
                             data.iosUpdateTime = temp.toString();
                         }
                         var QRCode = require('qrcode');
                         const pageUrl = `${serverPath}release/${data.appPackageName}/index.html`;
-                        QRCode.toDataURL(pageUrl,function(err,url){
-                            if(err){
+                        QRCode.toDataURL(pageUrl, function (err, url) {
+                            if (err) {
                                 console.log(err);
                                 return err;
                             }
                             data.url = url;
                             const result = compiled(data);
                             fs.outputFile(`yigomobile/public/release/${data.appPackageName}/index.html`, result, (err) => {
-                                if(err) {
+                                if (err) {
                                     console.log(err);
                                     return err;
                                 }
@@ -154,9 +159,9 @@ function monitor(){
                     })
                 })
         }
-        busy=false;
-    })
-        .catch(function(e,winston){
+        busy = false;
+    }).catch(function (e, winston) {
+        if(tt){
             tt.status = "rejected";
             tt.save();
             var path = require('path');
@@ -168,26 +173,28 @@ function monitor(){
             var err = e.toString();
             console.log(err)
             tt.winston.info(err);
-            busy=false;
-            // // 如果遇到Error code 1/2 错误，重启服务器
-            // if(/Error code 1/.test(err)){
-            //     process.exit(1);
-            // }
+        }
+        busy = false;
+        // TODO
+        // // 如果遇到Error code 1/2 错误，重启服务器
+        // if(/Error code 1/.test(err)){
+        //     process.exit(1);
+        // }
 
     });
 }
 
-var buildInterval =null;// setInterval(monitor,20*1000);
+var buildInterval = null;
 
 const packUtil = {
-    start : function () {
+    start: function () {
         monitor();
         buildInterval = setInterval(monitor, 20 * 1000);
     },
-    stop:function(){
+    stop: function () {
         clearInterval(buildInterval);
     },
-    isBusy:function(){
+    isBusy: function () {
         return busy;
     }
 }
